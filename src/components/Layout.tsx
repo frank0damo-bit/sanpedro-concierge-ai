27c52647bbdc47acd6084104e0a48e6cf8abdb48
alexkinnunen <alexkinnunen@hotmail.com>
2025-08-24 16:52:06 -0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,8 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import FloatingTripButton from "@/components/FloatingTripButton";
 import FooterCTA from "@/components/FooterCTA";
+import ScrollToTop from "./ScrollToTop";
 
 export function Layout() {
   const location = useLocation();
@@ -14,6 +14,7 @@ export function Layout() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Header />
+      <ScrollToTop />
       <main className="flex-grow pt-16">
         <Outlet />
       </main>
@@ -22,7 +23,6 @@ export function Layout() {
       {shouldShowFooterCTA && <FooterCTA />}
 
       <Footer />
-      <FloatingTripButton />
     </div>
   );
 }
