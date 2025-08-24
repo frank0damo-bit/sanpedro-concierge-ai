2aa293dce5b3f9be99ad939a1f175dd2c477fdbc
alexkinnunen <alexkinnunen@hotmail.com>
2025-08-23 21:09:48 -0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,8 @@ export function Layout() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Header />
-      {/* This main tag is crucial. It grows to fill all available space. */}
       <main className="flex-grow pt-16">
-        <Outlet /> {/* This renders the content of your individual pages */}
+        <Outlet />
       </main>
       <Footer />
     </div>
