0b751a3c4d1f134905def1619c59dea548696368
alexkinnunen <alexkinnunen@hotmail.com>
2025-08-24 14:17:57 -0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import BuildMyTripButton from "@/components/BuildMyTripButton";
+import FloatingTripButton from "@/components/FloatingTripButton";
 import FooterCTA from "./FooterCTA";
   
 export function Layout() {
