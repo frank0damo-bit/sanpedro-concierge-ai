65a9b113a3ef0e724451aecca6602cfd98568262
alexkinnunen <alexkinnunen@hotmail.com>
2025-08-24 12:53:55 -0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,8 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { FloatingTripButton } from "./FloatingTripButton";
-
+import { FooterCTA } from "./FooterCTA';
+  
 export function Layout() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
@@ -10,8 +11,10 @@ export function Layout() {
       <main className="flex-grow pt-16">
         <Outlet />
       </main>
+      <FooterCTA />
       <Footer />
       <FloatingTripButton /> {/* Add this line */}
+      
     </div>
   );
 }
